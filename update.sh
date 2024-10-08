d64a3d48f4c7e07ae3b0eb8decbcb679bf051ede
Jiarui Xu <jixu9182@uni.sydney.edu.au>
2024-10-09 00:48:01 +0800

    feat:update on 2024-10-08 (#9)
    * Test new features
    
    * Test new features
    
    * Test new features
    
    * Test new features
    
    * Test new features
    
    * Test new features
    
    * Test new features
    
    * feat:update on 2024-10-08

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ DATE=$(date "+%Y-%m-%d")
 BRANCH=$(git branch --show-current)
 
 git add .
-git commit -m "Update on $DATE" || exit
+git commit -m "feat:update on $DATE" || exit
 git push origin $BRANCH || exit
