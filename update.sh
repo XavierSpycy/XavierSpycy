1b0693e8709ff561cb50bbc6f90ea7376e8303c9
XavierSpycy <115918068+XavierSpycy@users.noreply.github.com>
2026-05-30 20:00:57 +0800

    chore(routine) update on 2026-05-30

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DATE=$(date "+%Y-%m-%d")
 BRANCH=$(git branch --show-current)
+# 如果没有传入参数，则使用默认 message；否则使用 $1
+MESSAGE=${1:-"chore(routine) update on $DATE"}
 
 git add .
-git commit -m "feat:update on $DATE" || exit
-git push origin $BRANCH || exit
+git commit -m "$MESSAGE" || exit
+git push origin "$BRANCH" || exit
